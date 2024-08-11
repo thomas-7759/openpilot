467919ff90069c870f3d1ac5cd3369c791531ad9
thomas-7759 <thomash7759@gmail.com>
2024-08-10 19:08:32 -0600

    Update crash.py

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -38,9 +38,9 @@ else:
     username = 'undefined'
 
   if smiskol_remote:  # CHANGE TO YOUR remote and sentry key to receive errors if you fork this fork
-    sentry_uri = 'https://a83947fe6772400bb220c3f0e4a6e63b@o237581.ingest.sentry.io/5252098'
+    sentry_uri = 'https://d544b36d2f81069aae01837c2fc53ef4@o4507756023906304.ingest.de.sentry.io/4507756026855504'
   else:
-    sentry_uri = 'https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924'  # stock
+    sentry_uri = 'https://d544b36d2f81069aae01837c2fc53ef4@o4507756023906304.ingest.de.sentry.io/4507756026855504'  # stock
   client = Client(sentry_uri, install_sys_hook=False, transport=HTTPTransport, release=version, tags=error_tags)
 
 
