5630df9aa53d4205fdda601cee47eb3bfe3ebaeb
thomas-7759 <thomash7759@gmail.com>
2024-09-01 12:11:56 -0600

    delete steer command

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -1,13 +1,3 @@
-def create_steer_command(packer, steer, steer_req, raw_cnt):
-  """Creates a CAN message for the Toyota Steer Command."""
-
-  values = {
-    "STEER_REQUEST": steer_req,
-    "STEER_TORQUE_CMD": steer,
-    "COUNTER": raw_cnt,
-    "SET_ME_1": 1,
-  }
-  return packer.make_can_msg("STEERING_LKA", 0, values)
 
 def create_new_steer_command(packer, mode, steer_delta, steer_tq, frame):
   """Creates a CAN message for the actuator STEERING_COMMAND"""
